38a5ac8d99d879a5443c95aead2b0b08bdf4f018
Patryk Konecki <uber@op.pl>
2017-10-18 11:27:11 +0200

    emtpy html+JS

--- app.js
+++ app.js
@@ -2,13 +2,11 @@
     var conn = new WebSocket('ws://localhost:8080/points');
 	conn.onmessage = function(e) { console.log(e.data); };
     conn.onopen = function(e) { conn.send('connect'); };
-	$('.plus').click(function()
-	{
+	$('.plus').click(function() {
 		console.log("Inkrementuje");
 		conn.send("increment");
 	});
-	$('.minus').click(function()
-	{
+	$('.minus').click(function() {
 		console.log("Dekrementuje");
 		conn.send("decrement");
 	});
