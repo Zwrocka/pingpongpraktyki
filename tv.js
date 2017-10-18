de0b3f6b38a5b4b609b3d26a293504b4d324f5f4
Patryk Konecki <uber@op.pl>
2017-10-18 14:43:35 +0200

    Cos mi nie pykło

--- tv.js
+++ tv.js
@@ -1,6 +1,13 @@
 var conn = new WebSocket('ws://localhost:8080/points');
 conn.onmessage = function(e) { console.log(e.data); };
 conn.onopen = function(e) { conn.send('connect'); };
+conn.onmessage= function(){
+	log(e.data.toString());
+}
+conn.onerror=fun ction(){
+	log("error")
+}
 $('scoreuptade')function(){
-conn.onmessage();
+	
+	conn.onmessage();
 }
