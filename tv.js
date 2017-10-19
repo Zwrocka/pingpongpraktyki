f92f2691781bafcbb3516790e02cf7b16d31c418
Patryk Konecki <uber@op.pl>
2017-10-19 11:46:04 +0200

    Web onmessage edit

--- tv.js
+++ tv.js
@@ -3,6 +3,16 @@ var initailScore = 0;
 WebSocket('ws://localhost:8080/points');
 conn.onmessage = function(e) { 
     console.log(e.data);
+   
+    if(i=null){
+      var i=0;
+    } 
+    else if(console.log(e.data)==score_update_inc){
+       i++; 
+    }
+    else if(console.log(e.data)==score_update_dec){
+        i--;
+    }
     // musisz nadsluchiwac na wiadomosc z backendu i updatowac UI .text()
     // TODO dodac logike
     
