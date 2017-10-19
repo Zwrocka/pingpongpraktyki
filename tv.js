12b28184aa6bce48d07db2b901bfb0f008386118
Patryk Konecki <uber@op.pl>
2017-10-19 11:21:59 +0200

    WEB:Add onmessage

--- tv.js
+++ tv.js
@@ -1,13 +1,22 @@
-var conn = new WebSocket('ws://localhost:8080/points');
-conn.onmessage = function(e) { console.log(e.data); };
-conn.onopen = function(e) { conn.send('connect'); };
-conn.onmessage= function(){
-	log(e.data.toString());
+var conn = new 
+var initailScore = 0;
+WebSocket('ws://localhost:8080/points');
+conn.onmessage = function(e) { 
+    console.log(e.data);
+    // musisz nadsluchiwac na wiadomosc z backendu i updatowac UI .text()
+    // TODO dodac logike
+    
+}; 
+
+conn.onopen = function(e) { 
+    conn.send('connect'); 
+};
+conn.onclose = function(e) { 
+    conn.send('disconnect');
+};    
+conn.onerror=function(){
+	console.log("error")
 }
-conn.onerror=fun ction(){
-	log("error")
-}
-$('scoreuptade')function(){
-	
-	conn.onmessage();
+conn.onmessage = function(e){
+    console.log(event)
 }
