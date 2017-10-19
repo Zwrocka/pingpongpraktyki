669127a758abd141f1261c4b16387f0320c496cd
Patryk Konecki <uber@op.pl>
2017-10-19 13:00:02 +0200

    WEB:Debug last uptade script

--- tv.js
+++ tv.js
@@ -2,15 +2,12 @@ var conn = new
 var initailScore = 0;
 WebSocket('ws://localhost:8080/points');
 conn.onmessage = function(e) { 
-    console.log(e.data);
-   
-    if(i=null){
-      var i=0;
-    } 
-    else if(console.log(e.data)==score_update_inc){
+    console.log(e.data); 
+    
+    if(e.data=='score_update_inc'){
        i++; 
     }
-    else if(console.log(e.data)==score_update_dec){
+    else if(e.data=='score_update_dec'){
         i--;
     }
     // musisz nadsluchiwac na wiadomosc z backendu i updatowac UI .text()
