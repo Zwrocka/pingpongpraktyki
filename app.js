1bf9c996e9e1e9994a7f7dc17ddcd47a8a73d415
Patryk Konecki <uber@op.pl>
2017-10-25 15:33:53 +0200

    Web:Demo version apk

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-	// Then some JavaScript in the browserr:
-    var conn = new WebSocket('ws://localhost:8080/points');
+	// Then some JavaScript in the browser:
+    var conn = new WebSocket('ws://10.67.199.58:8080/points');
     var initialScore = 0;
     var initialScoree = 0;
-	// conn.onmessage = function(e) { console.log(e.data); };
+// conn.onmessage = function(e) { console.log(e.data); };
     conn.onopen = function(e) { 
         console.log('connect'); 
     };
@@ -10,24 +10,48 @@
         initialScore++; 
         $('.scoreupdatex').text(initialScore);
 		console.log("Inkrementuje");
-		conn.send("incrementFirst");
+		conn.send(JSON.stringify({
+            action: 'incrementFirst',
+        }));
 	});
 	$('.minus').click(function() {
         initialScore--; 
         $('.scoreupdatex').text(initialScore);
 		console.log("Dekrementuje");
-		conn.send("decrementFirst");
+		conn.send(JSON.stringify({
+            action: 'decrementFirst',
+        }));
 	});
     //Druga strona
     $('.pluss').click(function() {
         initialScoree++; 
         $('.scoreupdatey').text(initialScoree);
 		console.log("Inkrementujee");
-		conn.send("incrementSecond");
+		conn.send(JSON.stringify({
+            action: 'incrementSecond',
+        }));
 	});
 	$('.minuss').click(function() {
         initialScoree--; 
         $('.scoreupdatey').text(initialScoree);
 		console.log("Dekrementujee");
-		conn.send("decrementSecond");
+		conn.send(JSON.stringify({
+            action: 'decrementSecond',
+            }));
 	});
+    $('.gamebutton').click(function(){
+        conn.send(JSON.stringify({
+            action: 'startGame',
+            data:{
+                team_one: $('.teamone').val(),   
+                team_two: $('.teamtwo').val()
+            }
+            }));
+        });
+    $('.gamebuttonend').click(function(){
+        conn.send(JSON.stringify({
+            action: 'stopGame',
+        }));
+        });
+    
+   
--- tv.js
+++ tv.js
@@ -1,19 +1,42 @@
-var conn = new WebSocket('ws://localhost:8080/points');
+var conn = new WebSocket('ws://10.67.199.58:8080/points');
 //$('button.first').click function(){
 //$('input.gds').
 //}
+
+function updateScoreOnUI (backendJSON) {
+    $('.scoreupdate').text(backendJSON.data.pointsone);
+    $('.scoreupdate2').text(backendJSON.data.pointstwo);
+    $('.teamone').text(backendJSON.data.team_one);
+    $('.teamtwo').text(backendJSON.data.team_two);
+}
 conn.onmessage = function(e) { 
     console.log(e.data); 
     var myObj = JSON.parse(e.data);
-    $('.scoreupdate').text(myObj.pointsone);
     
-    $('.scoreupdate2').text(myObj.pointstwo);
-    console.log("parsuje");
+    if (myObj.action == 'startGame') {
+        updateScoreOnUI(myObj)
+    } else if(myObj.action == 'scoreUpdate'){
+        $('.scoreupdate').text(myObj.data.pointsone);
+        $('.scoreupdate2').text(myObj.data.pointstwo);
+    } else if(myObj.action == 'initialScore'){
+        updateScoreOnUI(myObj)
+    } else if (myObj.action == 'sendHistory') {
+  //     $('.score').text(myObj.data.pointsone);
+    //   $('.scoree').text(myObj.data.pointstwo);
+      // $('.members').text(myObj.data.team_one);
+    //   $('.memberss').text(myObj.data.team_two);
+      //  debugger;
+    }
+
 };
 
     
 conn.onopen = function(e) {
-//    conn.send('connect'); 
+    var history = conn.send(JSON.stringify({
+        action: 'getHistory',
+    }));
+    
+   // debugger;
 };
 
 conn.onclose = function(e) { 
@@ -23,3 +46,13 @@ conn.onclose = function(e) {
 conn.onerror=function(e){
 	console.error(e)
 };
+//var p="sss";
+//var b= e.data.history.teamone;
+
+//var html = "<tr> <td> " + e.data.team_one + " vs " +  e.data.team_two //+ "</td> <td>" ;
+//$('table').append(html)
+//alert(a+b);
+
+
+  //$('table').text("<tr> <td> " + e.data.history.teamone + " vs " +  e.data.history.teamtwo + "</td><td>"+e.data.history.members +":"+e.data.history.memberss+"</td></tr>";);
+    //    console.log(myObj.data.history);
